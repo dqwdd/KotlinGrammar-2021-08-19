1a0def3894961f57a540970c1904e45a72af313e
유병재 <dqwdd@naver.com>
2021-08-19 10:52:07 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/kotlingrammar_2021_08_19/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.neppplus.kotlingrammar_2021_08_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        코틀린 문법 연습용 프로젝트(UI작업 안함)
+    }
+}
